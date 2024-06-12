51117f3326fd1e58e19c74ec553540554ab0532b
Empers0n_ <60817007+seva009@users.noreply.github.com>
2024-06-12 18:06:29 +0700

    add some optimisations

--- WiFi-jammer.ino
+++ WiFi-jammer.ino
@@ -3,9 +3,27 @@
 #include "esp_bt.h"
 #include "esp_wifi.h"
 
-uint16_t channels[28] = { //there are 1 main frequency and 2 sub frequency 
-  9,
-	12, //1
+void (*soft_reset)() = 0; //dirty hack :)
+void (*jamming)();        //pointer to jam func (depending on the connected nRF24L01(+))
+
+bool useHSPI = false;
+bool useVSPI = false;
+
+//char '\' in code breaking all art but in terminal it's normal
+static const char* logo = "                                                                                                 \n\
+ _____                              _____       _       _    _ _______ _     _                                              \n\
+|  ___|                            |  _  |     ( )     | |  | (_)  ___(_)   (_)                                             \n\
+| |__ _ __ ___  _ __   ___ _ __ ___| |/' |_ __ |/ ___  | |  | |_| |_   _     _  __ _ _ __ ___  _ __ ___   ___ _ __          \n\
+|  __| '_ ` _ \\| '_ \\ / _ \\ '__/ __|  /| | '_ \\  / __| | |/\\| | |  _| | |   | |/ _` | '_ ` _ \\| '_ ` _ \\ / _ \\ '__| \n\
+| |__| | | | | | |_) |  __/ |  \\__ \\ |_/ / | | | \\__ \\ \\  /\\  / | |   | |   | | (_| | | | | | | | | | | |  __/ |      \n\
+\\____/_| |_| |_| .__/ \\___|_|  |___/\\___/|_| |_| |___/  \\/  \\/|_\\_|   |_|   | |\\__,_|_| |_| |_|_| |_| |_|\\___|_|    \n\
+               | |                                                         _/ |                                             \n\
+               |_|                                                        |__/                                              \n\
+";
+
+static const uint8_t channels[28] = { //there are 1 main frequency and 2 sub frequency 
+  9, 
+  12, //1
   14,
 	17, //2
   20,
@@ -30,100 +48,103 @@ uint16_t channels[28] = { //there are 1 main frequency and 2 sub frequency
 	67, //12
   70,
 	72, //13
-  74,
-  75 //to not invoke ub in baseJamming()
+  74
 	//84,  //14
   //87         //so it's banned in my country and there is no reason to jam it
 };
 
-SPIClass *sp = nullptr;
-SPIClass *hp = nullptr;
+SPIClass* vspi = nullptr; //vspi 
+SPIClass* hspi = nullptr; //hspi
 
-RF24 radio(16, 15, 16000000);   //HSPI CAN SET SPI SPEED TO 16000000 BY DEFAULT ITS 10000000
-RF24 radio1(22, 21, 16000000);  //VSPI CAN SET SPI SPEED TO 16000000 BY DEFAULT ITS 10000000
+RF24 radio(16, 15, 16000000);  //you can chose your pins
+RF24 radio1(22, 21, 16000000); //you can chose your pins
 
-uint8_t flagHSPI = 0;   //HSPI// Flag variable to keep track of direction
-uint8_t flagVSPI = 0;  //VSPI// Flag variable to keep track of direction
-
-uint8_t chVSPI = 45;
-uint8_t chHSPI = 45;
-
-const int num_reps = 50;
-
-const uint8_t num_channels = 126;
-
-uint8_t values[num_channels];
-
-uint8_t* active = nullptr;
-
-void extendedJamming() { //works on wifi and bluetooth
-  (flagHSPI == 0)?chHSPI+=2:chHSPI-=2;
-  (flagVSPI == 0)?chVSPI+=4:chVSPI-=4;
-
-  if (flagHSPI == 0 && chHSPI >= 84) flagHSPI = 1;
-  if (flagHSPI != 0 && chHSPI <= 1) flagHSPI = 0;
-
-  if (flagVSPI == 0 && chVSPI >= 84) flagVSPI = 1;
-  if (flagVSPI != 0 && chVSPI <= 1 ) flagVSPI = 0;
-  //Serial.printf("chs: %d : %d\n", chHSPI, chVSPI);
-  radio.setChannel(chHSPI);
-  radio1.setChannel(chVSPI);
-}
-
-void baseJamming() {
+__attribute__((optimize("unroll-loops")))
+void wifi_bl_Jamming() { //works on bluetooth(but not always) and wifi more efficient then wifiJamming() uses VSPI & HSPI
   for (int i = 0; i < sizeof(channels) / 2; i++) {
     radio1.setChannel(channels[i]);
     radio.setChannel(channels[i * 2]);
   }
 }
 
-void setup() {
-  Serial.begin(115200);
-  esp_bt_controller_deinit();
-  esp_wifi_stop();
-  esp_wifi_deinit();
-  esp_wifi_disconnect();
-  initHP();
-  initSP();
+__attribute__((optimize("unroll-loops")))
+void wifi_bl_Jamming_V() { //works on bluetooth(but not always) and wifi more efficient then wifiJamming() uses VSPI & HSPI
+  for (int i = 0; i < sizeof(channels); i++) {
+    radio1.setChannel(channels[i]);
+  }
 }
 
-void initSP() {
-  sp = new SPIClass(VSPI);
-  sp->begin();
-  if (radio1.begin(sp)) {
-    Serial.println("SP Started !!!");
-    radio1.setAutoAck(false);
+__attribute__((optimize("unroll-loops")))
+void wifi_bl_Jamming_H() { //works on bluetooth(but not always) and wifi more efficient then wifiJamming() uses HSPI
+  for (int i = 0; i < sizeof(channels); i++) {
+    radio.setChannel(channels[i]);
+  }
+}
+
+void init_error() {
+  Serial.print("Couldn't init VSPI or HSPI !!!");
+  delay(1000);
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.print(logo);
+  esp_bt_controller_deinit();
+  esp_wifi_deinit();
+  initHSPI();
+  initVSPI();
+  if      (!(useHSPI || useVSPI)) jamming = &init_error;
+  else if (useHSPI && useVSPI)    jamming = &wifi_bl_Jamming;
+  else if (useVSPI)               jamming = &wifi_bl_Jamming_V;
+  else if (useHSPI)               jamming = &wifi_bl_Jamming_H;
+}
+
+void initVSPI() {
+  vspi = new SPIClass(VSPI);
+  vspi->begin();
+  if (radio1.begin(vspi)) {
+    useVSPI = true;
+    Serial.println("VSPI Started !!!");
     radio1.stopListening();
     radio1.setRetries(0, 0);
     radio1.setPALevel(RF24_PA_MAX, true);
     radio1.setDataRate(RF24_2MBPS);
     radio1.setCRCLength(RF24_CRC_DISABLED);
+    radio1.setAutoAck(false);
+    radio1.startConstCarrier(RF24_PA_MAX, chHSPI);
     radio1.printPrettyDetails();
-    radio1.startConstCarrier(RF24_PA_MAX, chVSPI);
   } else {
-    Serial.println("SP couldn't start !!!");
+    Serial.println("VSPI couldn't start !!!");
   }
 }
 
-void initHP() {
-  hp = new SPIClass(HSPI);
-  hp->begin();
-  if (radio.begin(hp)) {
-    Serial.println("HP Started !!!");
-    radio.setAutoAck(false);
+void initHSPI() {
+  hspi = new SPIClass(HSPI);
+  hspi->begin();
+  if (radio.begin(hspi)) {
+    useHSPI = true;
+    Serial.println("HSPI Started !!!");
     radio.stopListening();
     radio.setRetries(0, 0);
     radio.setPALevel(RF24_PA_MAX, true);
     radio.setDataRate(RF24_2MBPS);
     radio.setCRCLength(RF24_CRC_DISABLED);
-    radio.printPrettyDetails();
+    radio.setAutoAck(false);
     radio.startConstCarrier(RF24_PA_MAX, chHSPI);
+    radio.printPrettyDetails();
   } else {
-    Serial.println("HP couldn't start !!!");
+    Serial.println("HSPI couldn't start !!!");
   }
 }
 
+uint16_t timer = 0;
+
 void loop() {
-  baseJamming();
+  if (timer >= 16000) [[unlikely]] {
+    Serial.println("Resetting...");
+    soft_reset();
+  }
+  timer++;
+  jamming(); //or wifiJamming()
   delay(1);
 }
