4b5f11714f46c9411c371620a6cd881166481a65
Empers0n_ <60817007+seva009@users.noreply.github.com>
2024-06-13 21:52:51 +0700

    Update WiFi-jammer.ino

--- WiFi-jammer.ino
+++ WiFi-jammer.ino
@@ -1,3 +1,4 @@
+//project Dalet... DaL is beautiful
 #include "RF24.h"
 #include <SPI.h>
 #include "esp_bt.h"
@@ -5,12 +6,15 @@
 
 void (*soft_reset)() = 0; //dirty hack :)
 void (*work)();           //pointer to jam func (depending on the connected nRF24L01(+))
-
+uint8_t num_sub_freq = 2; //thats mean number of frequencies between left main frequency and right main frequency like this (4,{4.1),5,(5.1},6) in braces 1 main and 2 sub frequencies and for 4 sub frequency is 4.1 like for 5
+char vt = -1;
 bool useHSPI = false;
 bool useVSPI = false;
 
+uint8_t noise[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
 //char '\' in code breaking all art but in terminal it's normal
-static const char* logo = "                                                                                                 \n\
+inline const char* logo = "                                                                                                 \n\
  _____                              _____       _       _    _ _______ _     _                                              \n\
 |  ___|                            |  _  |     ( )     | |  | (_)  ___(_)   (_)                                             \n\
 | |__ _ __ ___  _ __   ___ _ __ ___| |/' |_ __ |/ ___  | |  | |_| |_   _     _  __ _ _ __ ___  _ __ ___   ___ _ __          \n\
@@ -21,7 +25,7 @@ static const char* logo = "                                                     
                |_|                                                        |__/                                              \n\
 ";
 
-static const uint8_t channels[28] = { //there are 1 main frequency and 2 sub frequency 
+static const uint8_t channels[] = { //there are 1 main frequency and 2 sub frequency 
   9, 
   12, //1
   14,
@@ -53,36 +57,38 @@ static const uint8_t channels[28] = { //there are 1 main frequency and 2 sub fre
   //87         //so it's banned in my country and there is no reason to jam it
 };
 
-SPIClass* vspi = nullptr; //vspi 
-SPIClass* hspi = nullptr; //hspi
+SPIClass* vspi = nullptr; //VSPI
+SPIClass* hspi = nullptr; //HSPI
 
 RF24 radio(16, 15, 16000000);  //you can chose your pins
 RF24 radio1(22, 21, 16000000); //you can chose your pins
 
 __attribute__((optimize("unroll-loops")))
-void wifi_bl_Jamming() { //works on bluetooth(but not always) and wifi more efficient then wifiJamming() uses VSPI & HSPI
+void wifi_Jamming() { //works on wifi uses VSPI & HSPI
   for (int i = 0; i < sizeof(channels) / 2; i++) {
     radio1.setChannel(channels[i]);
+    //radio1.write(noise, sizeof(noise));
     radio.setChannel(channels[i * 2]);
+    //radio.write(noise, sizeof(noise));
   }
 }
 
 __attribute__((optimize("unroll-loops")))
-void wifi_bl_Jamming_V() { //works on bluetooth(but not always) and wifi more efficient then wifiJamming() uses VSPI & HSPI
+void wifi_Jamming_V() { //works on wifi uses VSPI
   for (int i = 0; i < sizeof(channels); i++) {
     radio1.setChannel(channels[i]);
   }
 }
 
 __attribute__((optimize("unroll-loops")))
-void wifi_bl_Jamming_H() { //works on bluetooth(but not always) and wifi more efficient then wifiJamming() uses HSPI
+void wifi_Jamming_H() { //works on wifi uses HSPI
   for (int i = 0; i < sizeof(channels); i++) {
     radio.setChannel(channels[i]);
   }
 }
 
 void init_error() {
-  Serial.print("Couldn't init VSPI or HSPI !!!");
+  Serial.print("Can't init VSPI or HSPI !!!");
   delay(1000);
 }
 
@@ -91,12 +97,20 @@ void setup() {
   Serial.print(logo);
   esp_bt_controller_deinit();
   esp_wifi_deinit();
+  Serial.print("--------------------------------HSPI--------------------------------\n");
   initHSPI();
+  Serial.print("--------------------------------HSPI--------------------------------\n\n");
+  Serial.print("--------------------------------VSPI--------------------------------\n");
   initVSPI();
-  if      (!(useHSPI || useVSPI)) work = &init_error;
-  else if (useHSPI && useVSPI)    work = &wifi_bl_Jamming;
-  else if (useVSPI)               work = &wifi_bl_Jamming_V;
-  else if (useHSPI)               work = &wifi_bl_Jamming_H;
+  Serial.print("--------------------------------VSPI--------------------------------\n");
+  char num_ch[2];
+  itoa((sizeof(channels)-1)/num_sub_freq, num_ch, 10);
+  Serial.print("Jamming channels: 1-");
+  Serial.println(num_ch);
+  if      (!(useHSPI || useVSPI)) {work = &init_error;     Serial.println("Can't run VSPI or HSPI");}
+  else if (useHSPI && useVSPI)    {work = &wifi_Jamming;   Serial.println("Using VSPI and HSPI");   }
+  else if (useVSPI)               {work = &wifi_Jamming_V; Serial.println("Using VSPI");            }
+  else if (useHSPI)               {work = &wifi_Jamming_H; Serial.println("Using HSPI");            }
 }
 
 void initVSPI() {
@@ -111,7 +125,7 @@ void initVSPI() {
     radio1.setDataRate(RF24_2MBPS);
     radio1.setCRCLength(RF24_CRC_DISABLED);
     radio1.setAutoAck(false);
-    radio1.startConstCarrier(RF24_PA_MAX, 8);
+    radio1.startConstCarrier(RF24_PA_MAX, 5);
     radio1.printPrettyDetails();
   } else {
     Serial.println("VSPI couldn't start !!!");
@@ -130,7 +144,7 @@ void initHSPI() {
     radio.setDataRate(RF24_2MBPS);
     radio.setCRCLength(RF24_CRC_DISABLED);
     radio.setAutoAck(false);
-    radio.startConstCarrier(RF24_PA_MAX, 5);
+    radio.startConstCarrier(RF24_PA_MAX, 2);
     radio.printPrettyDetails();
   } else {
     Serial.println("HSPI couldn't start !!!");
@@ -145,6 +159,6 @@ void loop() {
   //   ESP.restart();
   // }
   // timer++;
-  for (uint8_t i = 0; i <= (uint8_t)-1>>1;i++) work(); //or wifiJamming()
+  for (uint8_t i = 0; i <= (uint8_t)vt>>1;i++) work();
   delay(1);
 }
